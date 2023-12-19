750360eb7588dc92ac7b235db815078c64a1004d
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-12-19 10:15:11 +0900

    Appbarを表示

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,4 +1,13 @@
 <template>
-	<router-view></router-view>
+	<v-app>
+		<v-app-bar>
+			<template v-slot:prepend>
+				<v-app-bar-nav-icon variant="text"></v-app-bar-nav-icon>
+			</template>
+		</v-app-bar>
+		<v-main>
+			<router-view></router-view>
+		</v-main>
+	</v-app>
 </template>
 
