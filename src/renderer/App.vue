811905eeaf7108fa8d8543c9aa6197c6f642217e
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-12-19 10:25:54 +0900

    Appbarを消してドロワーを常時表示

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,13 +1,35 @@
 <template>
 	<v-app>
-		<v-app-bar>
-			<template v-slot:prepend>
-				<v-app-bar-nav-icon variant="text"></v-app-bar-nav-icon>
-			</template>
-		</v-app-bar>
+		<v-navigation-drawer permanent absolute>
+			<v-list>
+				<v-list-item v-for="(view, i) in views" :key="i" :value="view" :to="view.to">
+					<v-list-item-title v-text="view.text"></v-list-item-title>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+
 		<v-main>
 			<router-view></router-view>
 		</v-main>
 	</v-app>
 </template>
+
+<script>
+export default {
+	data: () => ({
+		views: [
+			{
+				text: 'ホーム',
+				to: '/'
+			},
+			{
+				text: 'このアプリについて',
+				to: '/about'
+			}
+		]
+	})
+}
+</script>
+
+
 
