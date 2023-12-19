b0f8a84064fff4ba5604702290621d4f28559a88
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-12-19 10:14:44 +0900

    @mdi/fontを使用するように変更

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -3,9 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 import Top from './views/Top.vue'
@@ -32,10 +32,6 @@ const vuetify = createVuetify({
 	directives,
 	icons: {
 		defaultSet: 'mdi',
-		aliases,
-		sets: {
-			mdi
-		}
 	}
 })
 
