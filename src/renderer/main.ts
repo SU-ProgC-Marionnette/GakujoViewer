ae780bcd24f6d8acde4d87c066cdd47908b7619d
yuu528 <yuuta.kawaguchi28@gmail.com>
2023-12-19 09:33:06 +0900

    Vuetifyを使用するように変更

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import Top from './views/Top.vue'
 import About from './views/About.vue'
@@ -21,7 +26,13 @@ const router = createRouter({
 	routes
 })
 
+const vuetify = createVuetify({
+	components,
+	directives
+})
+
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(router)
 app.mount('#app')
