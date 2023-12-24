b0d1e82b979a1eff88b2b8aee44c6cde71b774cf
Yuu528 <yuu528mail@gmail.com>
2023-12-24 13:12:20 +0900

    Scraperで自動ログインするように

--- src/main/scraper/scraper.ts
+++ src/main/scraper/scraper.ts
@@ -1,5 +1,8 @@
 import puppeteer from 'puppeteer'
 
+import { FileUtil } from '../util/fileutil'
+import { AppBrowser } from './appbrowser'
+
 export class Scraper {
 	private url = 'https://gakujo.shizuoka.ac.jp/portal/'
 	private browser
@@ -9,27 +12,52 @@ export class Scraper {
 	}
 
 	public init = async(): Promise<void> => {
-		this.browser = await puppeteer.launch({
-			// ログインできないのでとりあえずheadless: false
-			// headless: 'new'
-			headless: false
-		})
-		this.page = await this.browser.newPage()
+		// CSSセレクタ
+		const gakujoHomeSel: string = '#home.new' // 学情のホーム画面検出
+		const msLoginSel: string = '#i0116' // ログイン画面検出
 
-		await this.page.goto(this.url)
+		const appBrowser = new AppBrowser()
+		let elmId: string = ''
 
-		// login
-		const loginBtnSel: string = '.btn_login'
-		await this.page.waitForSelector(loginBtnSel)
-		const loginBtn = await this.page.$(loginBtnSel)
-		await loginBtn.evaluate(btn => btn.click())
+		// 学情のホームに辿りつくまで試行
+		while(elmId == '' || gakujoHomeSel.indexOf(elmId) == -1) {
+			// 保存されたcookieを取得
+			let cookieStr: string = await FileUtil.read(FileUtil.LOGIN_COOKIE)
+			while(cookieStr == '') {
+				// Cookieが保存されてないときログイン画面を表示
+				await appBrowser.login()
+				cookieStr = await FileUtil.read(FileUtil.LOGIN_COOKIE)
+			}
 
-		// ログインは後で実装
+			this.browser = await puppeteer.launch({
+				headless: 'new'
+			})
+			this.page = await this.browser.newPage()
 
-		// トップページが読みこまれるのを待つ
-		await this.page.waitForSelector('#home.new', {
-			timeout: 0
-		})
+			await this.page.goto(this.url)
+
+			// 保存されてたcookieを復元
+			const cookies = JSON.parse(cookieStr)
+			await this.page.setCookie(...cookies)
+
+			// login
+			const loginBtnSel: string = '.btn_login'
+			await this.page.waitForSelector(loginBtnSel)
+			const loginBtn = await this.page.$(loginBtnSel)
+			await loginBtn.evaluate(btn => btn.click())
+
+			// トップページかログイン画面が読みこまれるのを待つ
+			const elmHandle = await this.page.waitForSelector([gakujoHomeSel, msLoginSel].join(','), {
+				timeout: 0
+			})
+			elmId = '#' + (await elmHandle.evaluate(elm => elm.id))
+
+			if(elmId == msLoginSel) {
+				// 自動ログインできなかったときログイン画面を表示
+				await this.browser.close()
+				await appBrowser.login()
+			}
+		}
 
 		return
 	}
