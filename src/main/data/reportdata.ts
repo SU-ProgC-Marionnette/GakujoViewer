3e90339bdf975e55ba13cd38231fd56df38a26b5
Yuu528 <yuu528mail@gmail.com>
2024-01-12 16:01:16 +0900

    DataのDateをnullableに

--- src/main/data/reportdata.ts
+++ src/main/data/reportdata.ts
@@ -3,10 +3,10 @@ import { ExpireStatus } from "./expirestatus";
 import { SubmitType } from "./submittype";
 
 export class ReportData extends ExpireData{
-    submit: Date;
+    submit: Date | null;
 
     constructor(subject:string, title:string,
-        status: ExpireStatus, start: Date, expire:Date, type: SubmitType,submit: Date){
+        status: ExpireStatus, start: Date | null, expire:Date | null, type: SubmitType,submit: Date | null){
         super(subject, title, status, start, expire, type);
         this.submit= submit;
     }
